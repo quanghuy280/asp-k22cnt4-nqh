5dc356dbc7ca0a04258fc693b360939f1e33fe67
Qhuy <nguyenquangh661@gmail.com>
2024-04-21 15:47:10 +0700

    lesson1

---
+++ lesson1/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.name = "Nguyễn Quang Huy";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
